342df7f5467289082ee62c94400f5b72730bef9b
Gui-lpz <marta.lopezmairena@ucr.ac.cr>
2026-01-22 10:58:08 -0600

    update entities

--- src/main/java/model/entities/Space.java
+++ src/main/java/model/entities/Space.java
@@ -1,15 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.entities;
+
 import model.entities.VehicleType;
-/**
- *
- * @author EstebanAntonioSanabr
- */
-
-
 
 public class Space {
 
@@ -84,8 +75,8 @@ public class Space {
     public void setVehicleType(VehicleType vehicleType) {
         this.vehicleType = vehicleType;
     }
+
+
+
+
 }
-
-
-
-
