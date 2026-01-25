00be5d9e7cc6d6daa239b7953bca65eb548befba
Darío Mora Piedra <morapiedradari32@gmail.com>
2026-01-25 00:15:48 -0600

    Merge master into Darío
    # Conflicts:
    #	target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst

--- src/main/java/model/entities/VehicleType.java
+++ src/main/java/model/entities/VehicleType.java
@@ -5,15 +5,16 @@ public class VehicleType {
     private String description;
     private byte numberOfTires;
     private float fee;
-
+    private String type; 
     public VehicleType() {
     }
 
-    public VehicleType(int id, String description, byte numberOfTires, float fee) {
+    public VehicleType(int id, String description, byte numberOfTires, float fee, String type) {
         this.id = id;
         this.description = description;
         this.numberOfTires = numberOfTires;
         this.fee = fee;
+        this.type = type;
     }
 
     public int getId() {
@@ -48,10 +49,19 @@ public class VehicleType {
         this.fee = fee;
     }
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     @Override
     public String toString() {
-        return "VehicleType{" + "id=" + id + ", description=" + description + ", numberOfTires=" + numberOfTires + ", fee=" + fee + '}';
+        return "VehicleType{" + "id=" + id + ", description=" + description + ", numberOfTires=" + numberOfTires + ", fee=" + fee + ", type=" + type + '}';
     }
+    
 
 
 }
